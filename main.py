f9ce9585d294a658c4a27a61cd25726d57d85e1c
Vytenis Dambrauskas <vytenisdambrauskas@gmail.com>
2023-04-18 16:42:15 +0300

    commit

--- main.py
+++ main.py
@@ -6,7 +6,10 @@ from game_data import data
 def get_random_comparison():
     return random.choice(data)
 
-compare_A = get_random_comparison()
-print(f" A is: {compare_A['name']}")
-compare_B = get_random_comparison()
-print(f" B is: {compare_B['name']}")
+A = get_random_comparison()
+print(f" Compare A: {A['name']}, a {A['description']} from {A['country']}")
+B = get_random_comparison()
+print(f" Compare B: {B['name']}, a {B['description']} from {B['country']}")
+
+answer = input("Who has more followers? Type 'A' or 'B'?")
+print(type(answer))
